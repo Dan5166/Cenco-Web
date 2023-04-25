5629749d78803bbbc334958b5579f8f73e3ceecf
Dan5166 <d.vsquezojeda@uandresbello.edu>
2023-04-25 11:47:32 -0400

    Commit listo para deploy con accesos optimizados -Accesos de roles firestore-- + sidebar dinámico por roles, falta limpieza css y migración de Firebase --Está en xperritogato actualmente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    projectId: 'cenco-web-prot',
-    appId: '1:746930550295:web:31975f6942007f51808e52',
-    storageBucket: 'cenco-web-prot.appspot.com',
-    apiKey: 'AIzaSyBoh8K1c25jemJzPt_RG6xf7CzQOjJQFTM',
-    authDomain: 'cenco-web-prot.firebaseapp.com',
-    messagingSenderId: '746930550295',
-    measurementId: 'G-QNLZSQS6EC',
+    projectId: 'migracion-de-prueba',
+    appId: '1:617221388472:web:5044b032c5556549139c4d',
+    storageBucket: 'migracion-de-prueba.appspot.com',
+    apiKey: 'AIzaSyDvf12dCmoFjKmVSxF6uDMmtQCX0nTeI-k',
+    authDomain: 'migracion-de-prueba.firebaseapp.com',
+    messagingSenderId: '617221388472',
+    measurementId: 'G-398TW3ZZK4',
   },
   production: true
 };
