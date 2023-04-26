fea67aa4a3b94b44b25c6089ea07e0658de33e12
Dan5166 <d.vsquezojeda@uandresbello.edu>
2023-04-26 17:55:15 -0400

    Commit listo para deploy con accesos optimizados, falta limpieza css y adaptación para movil media -- cuenta firebase xperritogato

--- src/app/pages/learning-path/learning-path.component.ts
+++ src/app/pages/learning-path/learning-path.component.ts
@@ -11,17 +11,13 @@ export class LearningPathComponent {
       id:1,
       nombre:'Curso de Amazon Lambda I',
       descripcion:'Curso de Angular para principiantes',
-      imagen:'../../../assets/dist/img/AmazonLambda.png',
-      background1: 'background-color: #dd8b47; height: 250px; position: relative;',
-      background2: 'background-color: #e0cabc; height: 250px; position: relative;',
+      imagen:'../../../assets/dist/img/AmazonLambda.png'
     },
     {
       id:2,
       nombre:'Curso de Amazon Lambda I',
       descripcion:'Curso de Angular para principiantes',
-      imagen:'../../../assets/dist/img/Amazon-S3-Logo.svg',
-      background1: 'background-color: #c53333; height: 250px; position: relative;',
-      background2: 'background-color: #e0bcbc; height: 250px; position: relative;',
+      imagen:'../../../assets/dist/img/Amazon-S3-Logo.svg'
     },
   ]
 
